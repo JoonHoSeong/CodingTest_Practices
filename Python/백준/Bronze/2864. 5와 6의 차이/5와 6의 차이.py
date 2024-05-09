e172339f3ad8ba501cf2b74847ebdd7daf48c688
SeongJoonHo <87454608+JoonHoSeong@users.noreply.github.com>
2024-05-09 20:10:44 +0900

    [Bronze II] Title: 5와 6의 차이, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/2864. 5와 6의 차이/5와 6의 차이.py
@@ -0,0 +1,7 @@
+num1, num2 = input().split(' ')
+num1 = num1.replace('6', '5')
+num2 = num2.replace('6','5')
+print(int(num1)+int(num2), end=' ')
+num1 = num1.replace('5', '6')
+num2 = num2.replace('5','6')
+print(int(num1)+ int(num2))
